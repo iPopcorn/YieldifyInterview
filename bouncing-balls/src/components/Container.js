10c84b3e17430d728be1fdb089426e407e2ffebc
iPopcorn <taypetrillo@gmail.com>
2020-07-14 17:58:32 +0100

    Adds container component

---
+++ bouncing-balls/src/components/Container.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Container extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Container</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Container;
