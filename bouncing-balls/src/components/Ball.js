e867380676c0373d796374746a74fdfadabde78d
iPopcorn <taypetrillo@gmail.com>
2020-07-15 18:46:09 +0100

    Experiments with using simple quadratic functions as the path for the balls. Begins to simulate a parabolic path by manually changing direction after a set distance.

--- bouncing-balls/src/components/Ball.js
+++ bouncing-balls/src/components/Ball.js
@@ -7,15 +7,60 @@ class Ball extends Component {
             positionX: props.positionX,
             positionY: props.positionY,
             boundaries: props.boundaries,
-            movingForward: true
+            movingForward: true,
+            movingUp: true,
+            minimumDistance: 25
         }
     }
 
     componentDidMount() {
-        this.timerID = setInterval(() => this.getPosition(), 10);
+        this.animationID = setInterval(() => this.getPositionQuadratic(), 10);
+        console.dir(this.state.boundaries);
     }
 
-    getPosition() {
+    scaleY(rawNumber) {
+        const minY = this.state.boundaries.top;
+        const maxY = this.state.boundaries.bottom;
+        // minPossible = 0;
+        const maxPossible = Math.pow(this.state.boundaries.right, 2);
+
+        return (maxY - minY) * rawNumber / maxPossible + minY;
+    }
+
+    switchDirectionVertical() {
+        const threshold = 9;
+        const switchFactor = Math.floor((Math.random() * 10) + 1); // get a number between 1 and 10
+
+        // randomly switch direction at a certain threshold
+        return (switchFactor > threshold) ? !this.state.movingUp : this.state.movingUp;
+    }
+    
+    getPositionQuadratic() {
+        let newState = {...this.state}
+        let newPositionX = this.state.positionX + 1;
+        let newPositionY = this.state.positionY;
+
+        if (this.state.movingUp) {
+            newPositionY -= 2;
+
+            // randomly switch direction after a minimum distance has been traveled
+            (newState.minimumDistance > 0) ? newState.minimumDistance -= 1 : newState.movingUp = this.switchDirectionVertical();
+        } else {
+            newPositionY += 4;
+        }
+
+        // let newPositionY = (this.state.movingUp) ? this.state.positionY - 2 : this.state.positionY + 4;
+        // let newPositionY = this.scaleY(2 * (Math.pow(this.state.positionX, 2)));  // Apply simple -x^2 function and scale it
+
+        newState.positionX = newPositionX;
+        newState.positionY = newPositionY;
+
+        // console.log(`x: ${newPositionX}, y: ${newPositionY}`);
+
+        this.setState(newState);
+    }
+
+    getPositionHorizontal() {
         let newState = {...this.state}
         let newPositionX = (this.state.movingForward) ? this.state.positionX + 5 : this.state.positionX - 5;
 
--- bouncing-balls/src/components/Container.js
+++ bouncing-balls/src/components/Container.js
@@ -25,6 +25,8 @@ class Container extends Component {
         this.setState((state) => {
             state.boundaries = boundaries
         });
+
+        console.dir(boundaries);
     }
 
     createBall = (e) => {
