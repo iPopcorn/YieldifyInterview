f018b75402cd08f98fa119067372dd3d54f967c5
iPopcorn <taypetrillo@gmail.com>
2020-07-16 10:53:47 +0100

    Adds logic to stop the balls from moving once they are rolling on the ground. Removes the animation function from the event loop once the ball stops moving.

--- bouncing-balls/src/components/Ball.js
+++ bouncing-balls/src/components/Ball.js
@@ -11,6 +11,10 @@ class Ball extends Component {
         console.dir(this.state.boundaries);
     }
 
+    componentWillUnmount() {
+        clearInterval(this.animationID);
+    }
+
     /**
      * Sets the initial state by randomly choosing the direction and speed.
      * @param {object} props The props passed in to the constructor of this object.
@@ -18,18 +22,17 @@ class Ball extends Component {
     setInitialState(props) {
         const horizontalDirectionThreshold = 5;
         const verticalDirectionThreshold = 5;
-        const initialSpeed = this.setSpeed();
         
         return {
             positionX: props.positionX,
             positionY: props.positionY,
             boundaries: props.boundaries,
             movingForward: (this.getRandom() > horizontalDirectionThreshold) ? true : false,
-            // movingUp: (this.getRandom() > verticalDirectionThreshold) ? true : false,
-            movingUp: true,
-            initialSpeed: initialSpeed,
-            currentSpeed: initialSpeed,
-            distanceTraveled: 0
+            movingUp: (this.getRandom() > verticalDirectionThreshold) ? true : false,
+            currentSpeedY: this.setSpeed(),
+            currentSpeedX: this.setSpeed(),
+            distanceTraveled: 0,
+            isMoving: true
         }
     }
 
@@ -63,13 +66,35 @@ class Ball extends Component {
      * the new speed.
      * @param {object} state The current state of the ball
      */
-    updateSpeed(state) {
+    updateSpeedY(state) {
         let newSpeed;
 
         // Slow down if moving up, otherwise speed up
-        (state.movingUp) ? newSpeed = state.currentSpeed - 1 : newSpeed = state.currentSpeed + 1;
+        (state.movingUp) ? newSpeed = state.currentSpeedY - 1 : newSpeed = state.currentSpeedY + 1;
 
         return newSpeed;
+    }
+
+    /**
+     * Sets a new speed in the X direction. The speed is updated based on the position of the ball. It only reduces the speed 
+     * if the ball is rolling at the bottom of the container.
+     * @param {object} state The current state of the ball
+     */
+    updateSpeedX(state) {
+        // console.log(`Ball is at bottom? ${this.isAtBottom(state)}`);
+        // console.log(`Ball is moving up? ${state.movingUp}`);
+        // console.log(`Ball speed Y: ${state.currentSpeedY}`);
+        const speedYThreshold = 2;
+        if(
+            this.isAtBottom(state) && 
+            !state.movingUp && 
+            state.currentSpeedY < speedYThreshold && 
+            state.currentSpeedX > 0 && 
+            state.distanceTraveled % 20 === 0) {
+            return state.currentSpeedX - 1;
+        } else {
+            return state.currentSpeedX;
+        }
     }
     
     /**
@@ -78,30 +103,52 @@ class Ball extends Component {
      */
     setPosition() {
         let newState = {...this.state}
-        let newPositionX = (this.state.movingForward) ? this.state.positionX + 1 : this.state.positionX - 1;
+        let newPositionX = this.state.positionX;
         let newPositionY = this.state.positionY;
 
-        (this.state.movingUp) ? newPositionY -= this.state.currentSpeed : newPositionY += this.state.currentSpeed;
+        (this.state.movingForward) ? newPositionX += this.state.currentSpeedX : newPositionX -= this.state.currentSpeedX;
+        (this.state.movingUp) ? newPositionY -= this.state.currentSpeedY : newPositionY += this.state.currentSpeedY;
 
         newState.positionX = newPositionX;
         newState.positionY = newPositionY;
         newState.distanceTraveled += 1;
 
         if(newState.distanceTraveled % 5 === 0) {
-            newState.currentSpeed = this.updateSpeed(newState);
+            newState.currentSpeedY = this.updateSpeedY(newState);
+            newState.currentSpeedX = this.updateSpeedX(newState);
 
-            if(newState.currentSpeed === 0) {
+            if(newState.currentSpeedY === 0 && !this.isAtBottom(newState)) {
                 newState.movingUp = !newState.movingUp;
             }
         }
 
         newState = this.handleBoundaries(newState);
+        newState.isMoving = this.isMoving(newState);
 
         this.setState(newState);
     }
 
     /**
+     * Checks if the ball is at the bottom of the container
+     * @param {Object} state The current state of the ball
+     */
+    isAtBottom(state) {
+        return (state.boundaries.bottom - state.positionY === 1) ? true : false;
+    }
+
+    /**
+     * Checks if the ball is still moving. The ball is considered to be moving if either the Y speed is above the threshold 
+     * or the X speed is greater than 0.
+     * @param {object} state The current state of the ball
+     */
+    isMoving(state) {
+        const speedYThreshold = 2;
+        return (state.currentSpeedY < speedYThreshold && state.currentSpeedX === 0) ? false : true;
+    }
+
+    /**
      * Checks if the ball is at any of the container boundaries. Switches the direction and calculates a new speed if so.
+     * @param {object} state The current state of the ball
      */
     handleBoundaries(state) {
         // The minimum speed to calculate a bounce for. Using a lower threshold causes the ball to bounce forever.
@@ -114,18 +161,22 @@ class Ball extends Component {
 
         if(state.positionY <= state.boundaries.top) {
             newState.movingUp = !state.movingUp;
+            
+            // Set position just inside of the boundary so that the method doesn't get called again in case the position was outside of the boundary
+            newState.positionY = state.boundaries.top + 1;
         } else if(state.positionY >= state.boundaries.bottom) {
+            
             // Set position just inside of the boundary so that the method doesn't get called again in case the position was outside of the boundary
             newState.positionY = state.boundaries.bottom - 1;
             
-            if(Math.abs(state.currentSpeed) >= speedThreshold) {
+            if(Math.abs(state.currentSpeedY) >= speedThreshold) {
                 newState.movingUp = !state.movingUp;
 
                 // Reduce the speed by about 50% until the speed threshold is hit, then reduce the speed to 0
-                newState.currentSpeed = state.currentSpeed - Math.floor(state.currentSpeed / 2);
+                newState.currentSpeedY = state.currentSpeedY - Math.floor(state.currentSpeedY / 2);
             } else {
                 newState.movingUp = false;
-                newState.currentSpeed = 0;
+                newState.currentSpeedY = 0;
             }
         }
 
@@ -144,9 +195,13 @@ class Ball extends Component {
             left: this.state.positionX
         }
 
-        return (
-            <span style={myStyle}></span>
-        )
+        if(this.state.isMoving) {
+            return (
+                <span style={myStyle}></span>
+            )
+        } else {
+            return null;
+        }
     }
 
 }
