a725734190982a2a2d0171e6f3570433c2ac8ce3
iPopcorn <taypetrillo@gmail.com>
2020-07-17 13:16:56 +0100

    Updates

---
+++ precache-manifest.174b7e0c40327342f134e0b224efc2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f5a5b98e9aedf7cc045004a327bcdd",
+    "url": "/YieldifyInterview/index.html"
+  },
+  {
+    "revision": "22bc423b9e1202d550e7",
+    "url": "/YieldifyInterview/static/css/main.2fa4e22b.chunk.css"
+  },
+  {
+    "revision": "5b4c351bbef7178ce1ed",
+    "url": "/YieldifyInterview/static/js/2.ad006613.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YieldifyInterview/static/js/2.ad006613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bc423b9e1202d550e7",
+    "url": "/YieldifyInterview/static/js/main.076baa68.chunk.js"
+  },
+  {
+    "revision": "d225aacfc30d3af4a9e5",
+    "url": "/YieldifyInterview/static/js/runtime-main.1d819a22.js"
+  }
+]);
